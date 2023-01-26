568c0df9143aa3ee2596b7643fe6db59a825a2ca
WilsonGbedemah <gbe20001@byui.edu>
2023-01-26 05:23:48 -0700

    New changes

---
+++ chamber/scripts/script.js
@@ -0,0 +1,22 @@
+// this code is for the toogling of the harmburger menue in the small and medium view
+function toggleMenu(){ 
+    document.getElementById('primaryNav').classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open")
+    }
+    
+    const x = document.getElementById('hamburgerBtn')
+    x.onclick = toggleMenu
+
+// This code is the date showing in the header of the website//
+    const datefield = document.querySelector(".date");
+    const datefieldUK = document.querySelector("aside");
+
+// This code is for the copy right and last modification shown in the website
+let year = document.querySelector("#year");
+const currentYear = new Date().getFullYear();
+year.textContent = currentYear;
+let modify = document.querySelector("#lastmodified");
+const currentTime=new Date(document.lastModified);
+modify.textContent = document.lastModified;
+
+
