d6c857bf8dcc3955a41ab2061bd6777c958e386c
WilsonGbedemah <gbe20001@byui.edu>
2023-02-01 04:28:28 -0700

    New additions

---
+++ DOM/scripts/script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('list');
+
+button.addEventListener('click', function() {
+
+
+})
