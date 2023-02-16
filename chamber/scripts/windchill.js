b3b3cdf7a0c074cc2a100de5cac4f24a8b6d7593
WilsonGbedemah <gbe20001@byui.edu>
2023-02-16 03:21:40 -0700

    additions

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,12 +1,12 @@
 //Windchill JavaScript in fahrenheit//
-const t = parseFloat(document.getElementById("temperature").value);
-const s = parseFloat(document.getElementById("windspeed").value);
-const f = 35.74 + 0.6215*t - 35.75*(Math.pow(s,0.16)) + 0.4275*tempF*(Math.pow(s,0.16))
+const t = parseFloat(document.getElementById("temperature").innerHTML);
+const s = parseFloat(document.getElementById("windspeed").innerHTML);
+const f = 35.74 + 0.6215*t - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16))
 if (t<=50 && s>3.0){
         windchill=f.toFixed(2);
     }
 else{
-    windchill = "NA";
+    windchill = "N/A";
 }
 document.getElementById('windchill').innerHTML = windchill;
 
