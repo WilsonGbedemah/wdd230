83d58d77f38dcb84af9e50fd4c5ca990c6a42186
WilsonGbedemah <gbe20001@byui.edu>
2023-02-16 05:14:50 -0700

    new

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,6 @@
 //Windchill JavaScript in fahrenheit//
 const t = parseFloat(document.getElementById("temperature").innerHTML);
-const s = parseFloat(document.getElementById("windspeed").innerHTML);
+const s = parseFloat(document.getElementById("windSpeed").innerHTML);
 const f = 35.74 + 0.6215*t - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16))
 if (t<=50 && s>3.0){
         windchill=f.toFixed(2);
@@ -8,6 +8,6 @@ if (t<=50 && s>3.0){
 else{
     windchill = "N/A";
 }
-document.getElementById('windchill').innerHTML = windchill;
+document.getElementById('windChill').innerHTML = windchill;
 
 
