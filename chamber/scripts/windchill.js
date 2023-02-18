0c28b92c7aa53549549252ffe97742a7ec9a4bb6
Use your GitHub username <Enter your email address>
2023-02-17 19:50:57 -0700

    changes

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,13 @@
+//Windchill JavaScript in fahrenheit//
+const t = parseFloat(document.getElementById("temperature").innerHTML);
+const s = parseFloat(document.getElementById("windSpeed").innerHTML);
+const f = 35.74 + 0.6215*t - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16))
+if (t<=50 && s>3.0){
+        windchill=f.toFixed(2);
+    }
+else{
+    windchill = "N/A";
+}
+document.getElementById('windChill').innerHTML = windchill;
+
+
---
+++ lesson-7/scripts/script.js
@@ -0,0 +1,29 @@
+//selecting all images with data-src attribute//
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// Check to see if intersection Observer is supported //
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+//Loop through each img to check status and load if necessary//
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
+  
