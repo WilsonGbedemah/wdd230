c684e8bba195e8e6cae1344c29290fbf9fc2ca3f
WilsonGbedemah <gbe20001@byui.edu>
2023-03-05 03:47:43 -0700

    updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,66 +1,59 @@
-
-// Js code for creating card section with JavaScript//
 async function getbusinessesData() {
-    const response = await fetch("json/data.json");
-    const data = await response.json();
-    //console.table(data.businesses);
-    displayBusinesses(data.businesses);
+  const response = await fetch("JSON/data.json");
+  const data = await response.json();
+  //console.table(data.businesses);
+  displayBusinesses(data.businesses);
 }
 
 async function displayBusinesses(businesses) {
-    const cards = document.querySelector(".businessCards");
+  const cards = document.querySelector("div.cards");
 
-    businesses.forEach((business) => {
-        let card = document.createElement("section");
-        let logo = document.createElement("img");
-        let location = document.createElement("p");
-        let number = document.createElement("p");
-        let siteLink = document.createElement("a");
-        let company = document.createElement("p");
+  businesses.forEach((business) => {
+      let card = document.createElement("section");
+      let logo = document.createElement("img");
+      let location = document.createElement("p");
+      let number = document.createElement("p");
+      let siteLink = document.createElement("a");
+      let company = document.createElement("p");
 
-        location.textContent = business.address;
-        number.textContent = business.telephone;
+      location.textContent = business.address;
+      number.textContent = business.telephone;
 
-        company.textContent = business.companyName;
-        company.setAttribute("class", "businessName");
-        
-        siteLink.textContent = business.siteURL;
-        siteLink.setAttribute("href", business.siteURL)
+      company.textContent = business.companyName;
+      company.setAttribute("class", "businessName");
+      
+      siteLink.textContent = business.siteURL;
+      siteLink.setAttribute("href", business.siteURL)
 
-        logo.setAttribute("src", business.siteLogo);
-        logo.setAttribute("alt", `siteLink of ${business.companyName}`);
-        logo.setAttribute("loading", "lazy");
-        logo.setAttribute("width", "456");
-        logo.setAttribute("height", "250");
-        logo.setAttribute("class", "businessLogo");
+      logo.setAttribute("src", business.logo);
+      logo.setAttribute("alt", `logo of ${business.companyName}`);
+      logo.setAttribute("loading", "lazy");
+      logo.setAttribute("width", "340");
+      logo.setAttribute("height", "440");
+      logo.setAttribute("class", "businessLogo");
 
-        card.appendChild(logo);
-        card.appendChild(company);
-        card.appendChild(location);
-        card.appendChild(number);
-        card.appendChild(siteLink);
+      card.appendChild(logo);
+      card.appendChild(company);
+      card.appendChild(location);
+      card.appendChild(number);
+      card.appendChild(siteLink);
 
-        cards.appendChild(card);
-    })
+      cards.appendChild(card);
+  })
 }
 
 getbusinessesData();
 
-// Js code for switching between card view and list view//
-document.addEventListener("DOMContentLoaded", () => {
-    const gridBtn = document.getElementById("grid");
-    gridBtn.addEventListener('click', ()=>{
-      document.getElementById("directory").classList.remove('list');
-      document.getElementById("directory").classList.add('grid');
-      listBtn.classList.remove('btn-active');
-      gridBtn.classList.add('btn-active');
-    });
-    const listBtn = document.getElementById("list");
-    listBtn.addEventListener('click', ()=>{
-      document.getElementById("directory").classList.remove('grid');
-      document.getElementById("directory").classList.add('list');
-      listBtn.classList.add('btn-active');
-      gridBtn.classList.remove('btn-active');
-    });
-  });
+function toggleLayout(){
+  if (document.querySelector(".listBtn")){
+      document.getElementById("directory-main").setAttribute("class", "directory-list");
+      document.getElementById("layoutBtn").setAttribute("class", "gridBtn");
+  }
+  else {
+      document.getElementById("directory-main").setAttribute("class", "directory-grid");
+      document.getElementById("layoutBtn").setAttribute("class", "listBtn");
+  }
+}
+
+document.getElementById("layoutBtn").addEventListener("click", toggleLayout);
 
