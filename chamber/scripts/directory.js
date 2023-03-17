1067029012acd903bb711dbd18ffdcc784436cfc
WilsonGbedemah <gbe20001@byui.edu>
2023-03-17 04:37:58 -0600

    updates

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -48,10 +48,12 @@ function toggleLayout(){
   if (document.querySelector(".listBtn")){
       document.getElementById("directory-main").setAttribute("class", "directory-list");
       document.getElementById("layoutBtn").setAttribute("class", "gridBtn");
+      document.getElementById("layoutBtn").innerHTML = "Grid";
   }
   else {
       document.getElementById("directory-main").setAttribute("class", "directory-grid");
       document.getElementById("layoutBtn").setAttribute("class", "listBtn");
+      document.getElementById("layoutBtn").innerHTML = "📋List";
   }
 }
 
