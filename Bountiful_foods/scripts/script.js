e27bfbebd094f0e7d7acbc378d7e18a0a6be51e2
WilsonGbedemah <gbe20001@byui.edu>
2023-03-29 03:50:44 -0600

    New project

---
+++ Bountiful_foods/scripts/script.js
@@ -0,0 +1,13 @@
+/**Harmburger menu JS**/
+function toggleMenu(){ 
+    document.getElementById('primaryNav').classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open")
+    }
+    
+    const x = document.getElementById('hamburgerBtn')
+    x.onclick = toggleMenu
+
+    /**Last modification JS**/
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+document.getElementById("lastmodified").textContent = document.lastModified
