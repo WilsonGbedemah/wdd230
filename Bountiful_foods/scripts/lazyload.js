1b6a8593d8664e5414939a3f6117212e30b26dde
WilsonGbedemah <gbe20001@byui.edu>
2023-03-29 07:13:18 -0600

    updates

--- Bountiful_foods/scripts/lazyload.js
+++ Bountiful_foods/scripts/lazyload.js
@@ -26,3 +26,25 @@ if ("IntersectionObserver" in window) {
       loadImages(img);
     });
   }
+
+  /*Number of User Visits*/
+let daysBetweenVisits = Number(window.localStorage.getItem("last-visit-ls"));
+
+let timeStamp = Date.now();
+let findDaysBetween = timeStamp;
+
+if (daysBetweenVisits !== 0){
+    findDaysBetween -= daysBetweenVisits;
+    findDaysBetween = Math.round(findDaysBetween / (1000*60*60*24));
+    if (findDaysBetween < 1){
+        document.getElementById("daysBetweenVisits").innerHTML = `No days have passed since your last visit`;
+    }
+    else{
+        document.getElementById("daysBetweenVisits").innerHTML = `It has been ${findDaysBetween} days since your last visit`;
+    }
+}
+else {
+    document.getElementById("daysBetweenVisits").innerHTML = "Join us again soon!";
+}
+
+localStorage.setItem("last-visit-ls", timeStamp)
