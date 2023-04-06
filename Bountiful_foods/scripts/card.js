3e59a76ac69330edca3e19cd1b456c2c49dd91dd
WilsonGbedemah <gbe20001@byui.edu>
2023-04-05 21:58:07 -0600

    updates

--- Bountiful_foods/scripts/card.js
+++ Bountiful_foods/scripts/card.js
@@ -1,20 +1,12 @@
-window.onload = function() {
-  // Get the form element
-  const form = document.getElementById('fill');
+// Get the current number of specialty drinks from localStorage
+let numSpecialtyDrinks = parseInt(localStorage.getItem('numSpecialtyDrinks')) || 0;
 
-  // Add a submit event listener to the form
-  form.addEventListener('submit', function(event) {
-    // Get the current user's email from the form
-    const email = form.elements.email.value;
+// Increment the number of specialty drinks
+numSpecialtyDrinks++;
 
-    // Get the current user's previous number of specialty drinks from local storage, or default to 0
-    let numDrinks = parseInt(localStorage.getItem(email)) || 0;
+// Store the new number of specialty drinks in localStorage
+localStorage.setItem('numSpecialtyDrinks', numSpecialtyDrinks);
 
-    // Get the number of specialty drinks that the user just submitted
-    const numNewDrinks = parseInt(form.elements.fruits.value) + parseInt(form.elements.fruits1.value);
-
-    // Update the total number of specialty drinks for the user
-    numDrinks += numNewDrinks;
-    localStorage.setItem(email, numDrinks);
-  });
-};
+// Display the number of specialty drinks on the information card
+let drinkCount = document.getElementById('drink-count');
+drinkCount.textContent = numSpecialtyDrinks;
