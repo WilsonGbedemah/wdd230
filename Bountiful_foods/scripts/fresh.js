4796146da4647b6b2e07e341dc59cc6a1954c2a4
WilsonGbedemah <gbe20001@byui.edu>
2023-04-11 03:56:00 -0600

    updates

--- Bountiful_foods/scripts/fresh.js
+++ Bountiful_foods/scripts/fresh.js
@@ -1,57 +1,79 @@
-async function getfruitsData() {
-    const response = await fetch("./scripts/fruit.json");
-    const data = await response.json();
-    const mp = new Map();
-    data.forEach((item) => {
-      mp.set(item.name.toUpperCase(), {
-        nutritions: item.nutritions,
-      });
+async function getFruitsData() {
+  const response = await fetch("./scripts/fruit.json");
+  const data = await response.json();
+  const mp = new Map();
+  data.forEach((item) => {
+    mp.set(item.name.toUpperCase(), {
+      nutritions: item.nutritions,
     });
-  
-    return mp;
-  }
-  
-  const productForm = document.getElementById("fill");
-  productForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const data = new FormData(productForm);
-    const firstName = data.get("fname");
-    const email = data.get("email");
-    const number = data.get("phone");
-    const firstFruit = data.get("fruits");
-    const secondFruit = data.get("fruits1");
-    const thirdFruit = data.get("fruits2");
-    const instructions = data.get("instructions");
-  
-    getfruitsData().then((data) => {
-      const display = document.getElementById("output");
-      const output = `
-        <h2>Personal Information</h2>
-        <p><strong>Name:</strong> ${firstName}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Phone:</strong> ${number}</p>
-  
-        <h2>Fruit Selections</h2>
-        <ul>
-          <li>
-            <p><strong>First fruit selected:</strong> ${firstFruit}</p>
-            <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
-              data.get(firstFruit.toUpperCase())
-            )}</p>
-          </li>
-          <li>
-            <p><strong>Second fruit selected:</strong> ${secondFruit}</p>
-            <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
-              data.get(secondFruit.toUpperCase())
-            )}</p>
-          </li>
-          <li>
-            <p><strong>Third fruit selected:</strong> ${thirdFruit}</p>
-            <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
-              data.get(thirdFruit.toUpperCase())
-            )}</p>
-          </li>
-        </ul>
+  });
+
+  return mp;
+}
+
+const productForm = document.getElementById("fill");
+productForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(productForm);
+  const firstName = data.get("fname");
+  const email = data.get("email");
+  const number = data.get("phone");
+  const firstFruit = data.get("fruits");
+  const secondFruit = data.get("fruits1");
+  const thirdFruit = data.get("fruits2");
+  const instructions = data.get("instructions");
+
+  getFruitsData().then((data) => {
+    const display = document.getElementById("output");
+    const firstFruitNutrition = data.get(firstFruit.toUpperCase());
+    const secondFruitNutrition = data.get(secondFruit.toUpperCase());
+    const thirdFruitNutrition = data.get(thirdFruit.toUpperCase());
+
+    const totalNutrition = {
+      carbohydrates: firstFruitNutrition.nutritions.carbohydrates +
+        secondFruitNutrition.nutritions.carbohydrates +
+        thirdFruitNutrition.nutritions.carbohydrates,
+      protein: firstFruitNutrition.nutritions.protein +
+        secondFruitNutrition.nutritions.protein +
+        thirdFruitNutrition.nutritions.protein,
+      fat: firstFruitNutrition.nutritions.fat +
+        secondFruitNutrition.nutritions.fat +
+        thirdFruitNutrition.nutritions.fat,
+      sugar: firstFruitNutrition.nutritions.sugar +
+        secondFruitNutrition.nutritions.sugar +
+        thirdFruitNutrition.nutritions.sugar,
+      calories: firstFruitNutrition.nutritions.calories +
+        secondFruitNutrition.nutritions.calories +
+        thirdFruitNutrition.nutritions.calories,
+    };
+
+    const output = `
+      <h2>Personal Information</h2>
+      <p><strong>Name:</strong> ${firstName}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Phone:</strong> ${number}</p>
+
+      <h2>Fruit Selections</h2>
+      <ul>
+        <li>
+          <p><strong>First fruit selected:</strong> ${firstFruit}</p>
+          <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
+            firstFruitNutrition
+          )}</p>
+        </li>
+        <li>
+          <p><strong>Second fruit selected:</strong> ${secondFruit}</p>
+          <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
+            secondFruitNutrition
+          )}</p>
+        </li>
+        <li>
+          <p><strong>Third fruit selected:</strong> ${thirdFruit}</p>
+          <p><strong>Fruit nutrients:</strong> ${JSON.stringify(
+            thirdFruitNutrition
+          )}</p>
+        </li>
+      </ul
   
         <h2>Special Instructions</h2>
         <p>${instructions}</p>
@@ -59,7 +81,6 @@ async function getfruitsData() {
         <h2>Date</h2>
         <p>${new Date().toDateString()}</p>
       `;
-      display.innerHTML = output;
-    });
+    display.innerHTML = output;
   });
-  
+});
--- Bountiful_foods/scripts/weather.js
+++ Bountiful_foods/scripts/weather.js
@@ -37,15 +37,26 @@ async function apiFetch() {
   }
 
     // Call the three-day forecast API to get forecast data
-    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&appid=cd716e3725143ae8cd86b17c41e03a2f';
-    fetch(forecastUrl)
-      .then((response) => response.json())
-      .then((data) => {
-        const forecast = data.list;
-        forecast.forEach((day, index) => {
-          const temp = day.temp.day.toFixed(0) + "°F";
-          const condition = day.weather[0].description;
-          document.getElementById(`day${index+1}`).textContent = `${temp}, ${condition}`;
-        });
-      })
-      .catch((error) => console.log(error));
+   // Fetch the three-day forecast data from the OpenWeatherMap API
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&appid=cd716e3725143ae8cd86b17c41e03a2f')
+.then(response => response.json())
+.then(data => {
+    // Extract the temperature data for each of the next three days
+    const day1Temp = data.list[0].main.temp;
+    const day2Temp = data.list[8].main.temp;
+    const day3Temp = data.list[16].main.temp;
+
+    // Convert the temperature values from Kelvin to Fahrenheit
+    const day1TempF = ((day1Temp - 273.15) * 1.8 + 32).toFixed(0);
+    const day2TempF = ((day2Temp - 273.15) * 1.8 + 32).toFixed(0);
+    const day3TempF = ((day3Temp - 273.15) * 1.8 + 32).toFixed(0);
+
+    // Display the temperature values in the HTML document
+    document.getElementById('day1').textContent = "Day 1"  + " " + "=" + " " + day1TempF + '°F';
+    document.getElementById('day2').textContent = "Day 2"  + " " + "=" + " " + day2TempF + '°F';
+    document.getElementById('day3').textContent = "Day 3"  + " " + "=" + " " + day3TempF + '°F';
+})
+.catch(error => {
+    console.error(error);
+});
+
